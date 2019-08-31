44e799186c4542f0492750d8fb74d96a12bfa15a
JackM400 <jack.millar400@gmail.com>
2019-08-31 22:17:19 +0100

    screen attribute - border -- pen to draw border

--- main.py
+++ main.py
@@ -8,11 +8,19 @@ import winsound
 import time
 
 isRunning = True
+
 # game screen
 gameWindow = turtle.Screen()
 gameWindow.title("Space Invaders - JackM400")
 gameWindow.setup(width=900, height=700)
 gameWindow.bgcolor("black")
+
+# screen attributes
+# -border
+borderBuilder = turtle.Turtle()
+borderBuilder.speed(0)  # 0 == fastest speed
+borderBuilder.color("white")
+
 
 while isRunning:
     gameWindow.update()
