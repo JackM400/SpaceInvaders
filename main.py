bc37a96e619dcd864cee6769e4611a268cb271f9
JackM400 <jack.millar400@gmail.com>
2019-08-31 22:13:50 +0100

    display screen

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 # Space Invaders
 # JackM400
 # jack.millar400@gmail.com
+
+import os
+import turtle
+import winsound
+import time
+
+isRunning = True
+# game screen
+gameWindow = turtle.Screen()
+gameWindow.title("Space Invaders - JackM400")
+gameWindow.setup(width=900, height=700)
+gameWindow.bgcolor("black")
+
+while isRunning:
+    gameWindow.update()
